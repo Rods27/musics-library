ef3dff7bb44a894d1637bc9a8b0b85615d58b82c
Rodrigo Leite <p22.rodrigo@webmotors.com.br>
2025-07-14 08:14:49 -0300

    feat(migrate-to-ts): Install Prettier/Eslint/Ts/vite, only run project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import GlobalStyle from './styles/global'
 import history from './utils/history';
 
-function App() {
+export default function App() {
   return (
-    <Provider store={ store }>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+    // <Provider store={ store }>
+    // </Provider>
+      <BrowserRouter>
         <GlobalStyle />
         <Routes history={ history }/>
       </BrowserRouter>
-    </Provider>
   );
 }
-
-export default App;
